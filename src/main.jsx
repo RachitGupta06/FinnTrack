80ddd7b7fae5a5332f8a28ca84c8bf0190f86799
RachitGupta06 <118665715+RachitGupta06@users.noreply.github.com>
2025-03-22 13:57:22 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = process.env.VITE_CLERK_PUBLISHABLE_KEY
 if (!PUBLISHABLE_KEY){
   throw new Error('Add your CLERK Publishable key to the .env file')
 }
